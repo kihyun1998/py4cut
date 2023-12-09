c11ea0cafe479ea9e422efe2e3642ec9f3c09bf4
pentakihyun <71832733+kihyun1998@users.noreply.github.com>
2023-12-09 17:52:33 +0900

    수정중

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+비디오와 이미지 같은 폴더에 저장하기
+"""
+
 import cv2
 import datetime
 from pygame import mixer
@@ -6,12 +10,18 @@ import time
 import os
 
 # 카운트다운 함수
-# test=2 실제=10
+# test=3, 사용=10
 def countdown():
-    for i in range(3, 0, -1):
+    count=3
+    # count=10
+    for i in range(count, 0, -1):
         print(i)
         time.sleep(1)
     print("사진을 찍습니다.")
+
+# 영상녹화 함수
+def record_video(out,frame):
+    out.write(frame)
 
 def record_video_and_capture_image():
     cap = cv2.VideoCapture(0)
@@ -21,9 +31,8 @@ def record_video_and_capture_image():
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
     # 영상 저장을 위한 코덱 및 해상도 설정
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    fourcc = cv2.VideoWriter_fourcc(*'DIVX')
     out = None
-    video_count = 0
     is_recording = False
 
     # 사운드 설정
@@ -40,6 +49,12 @@ def record_video_and_capture_image():
 
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
+            if is_recording:
+                print("녹화 종료")
+                out.release()
+                is_recording=False
+            cap.release()
+            cv2.destroyAllWindows()
             break
         
 
@@ -49,11 +64,21 @@ def record_video_and_capture_image():
         # 사진 이름 예시: Picture_1.png, Picture_2.png...
         elif key == ord('c'):
             img_count = 1
-            # 폴더이름 지정(날짜) 및 생성
-            img_folder = f"./image/{datetime.datetime.now().strftime('%Hh-%Mmin-%Ssec')}"
-            os.makedirs(img_folder,exist_ok=True)
 
-            for i in range(4):
+            print("녹화 시작")
+
+            # 저장폴더 지정 및 생성
+            save_folder = f"./video&image/{datetime.datetime.now().strftime('%Hh-%Mmin-%Ssec')}"
+            os.makedirs(save_folder,exist_ok=True)
+            
+            video_name = f"record_video.mp4" # 영상 파일 이름
+            video_savePath = f"./{save_folder}/{video_name}" # 영상 저장 경로
+            
+            # 녹화 시작
+            out = cv2.VideoWriter(video_savePath, fourcc, 40.0, (int(cap.get(3)), int(cap.get(4))))
+            is_recording = True # 녹화 중
+
+            for _ in range(4):
                 t = threading.Thread(target=countdown)
                 t.start()
                 while t.is_alive():
@@ -66,34 +91,18 @@ def record_video_and_capture_image():
                 shutter_sound.play()
 
                 img_name = f"Picture_{img_count}.png" # 이미지 이름
-                img_savePath = f"{img_folder}/{img_name}" # 이미지 저장 경로
+                img_savePath = f"{save_folder}/{img_name}" # 이미지 저장 경로
 
                 # 사진 저장
                 cv2.imwrite(img_savePath, frame)
                 print(f"{img_savePath} 사진 저장 완료!")
                 img_count += 1
-        
-        # 처음 r을 누르면 녹화가 시작하고
-        # 녹화 중 다시 r을 누르면 녹화가 종료된다.
-        elif key == ord('r'):
+
+            # 녹화중이라면
             if is_recording:
                 print("녹화 종료")
                 out.release()
-                is_recording = False
-            else:
-                print("녹화 시작")
-
-                # 영상 폴더 지정 및 생성
-                video_folder = f"./video/{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}" 
-                os.makedirs(video_folder,exist_ok=True)
-                
-                video_name = f"Video_{video_count}.mp4" # 영상 파일 이름
-                video_savePath = f"./{video_folder}/{video_name}" # 영상 저장 경로
-                
-                out = cv2.VideoWriter(video_savePath, fourcc, 20.0, (int(cap.get(3)), int(cap.get(4))))
-                is_recording = True
-                video_count += 1
-
+                is_recording = False # 녹화종료
         if is_recording:
             out.write(frame)
 
